4394dfd3062b2042b5bb095687c040fcaa8324d5
Parth <65162182+pbisen@users.noreply.github.com>
2025-05-02 00:42:39 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "pbisen.github.io/build-in-public",
+    baseUrl: "pbisen.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
